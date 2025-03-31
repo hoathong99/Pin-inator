d52838ee2b9171ff9a8e7ff9288f9bcadfb21dc6
Vu Ngoc Anh <hoathong99@gmail.com>
2025-03-31 09:41:07 +0700

    Add JWT

---
+++ src/main/java/com/example/Pin_inator/Security/UserInfo.java
@@ -0,0 +1,23 @@
+package com.example.Pin_inator.Security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
+}
